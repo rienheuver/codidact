4470f711dc4782d6fe2e33eb425a74d7872b0a75
Rien Heuver <rienheuver@gmail.com>
2021-05-27 12:36:52 +0200

    Adds videos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-import './scss/app.scss';
+import "./scss/app.scss";
+
+window.prevSlide = () => {
+  let current = document.querySelector(".slide-active");
+  let prev = document.querySelector(".slide-active").previousElementSibling;
+  if (prev !== null) {
+    current.className = "slide slide-next";
+    prev.className = "slide slide-active";
+  }
+}
+
+window.nextSlide = () => {
+  let current = document.querySelector(".slide-active");
+  let next = document.querySelector(".slide-active").nextElementSibling;
+  if (next !== null) {
+    current.className = "slide slide-prev";
+    next.className = "slide slide-active";
+  }
+}
